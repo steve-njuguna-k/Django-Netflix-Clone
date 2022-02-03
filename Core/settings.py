f6e145e04b3ec572523191b9160da4c1b1679d6c
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-03 19:13:48 +0300

    Styled Landing Page

--- Core/settings.py
+++ Core/settings.py
@@ -127,16 +127,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Media Files
 MEDIA_URL = '/media/'

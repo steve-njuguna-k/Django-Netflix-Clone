1bca0e13d8efabca7db91205904b94e8263dfcae
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-03 20:40:19 +0300

    Added login & sign up functions

--- Netflix/views.py
+++ Netflix/views.py
@@ -1,11 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 # Create your views here.
 def Home(request):
     return render(request, 'Index.html')
-
+    
 def Register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, '⚠️ Passwords Do Not Match! Try Again')
+            return redirect('Register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, '⚠️ Username Already Exists! Choose Another One')
+            return redirect('Register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, '⚠️ Email Address Already Exists! Choose Another One')
+            return redirect('Register')
+
+        user = User.objects.create_user(username=username, email=email)
+        user.set_password(password1)
+        user.save()
+
+        messages.success(request, '✅ Regristration Successful! You can now log in.')
+        return redirect('Register')
+
     return render(request, 'Register.html')
 
 def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if not User.objects.filter(username=username).exists():
+            messages.error(request, '⚠️ Username Does Not Exist! Choose Another One')
+            return redirect('Login')
+
+        if user is None:
+            messages.error(request, '⚠️ Username or Password Is Incorrect!! Please Try Again')
+            return redirect('Login')
+
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('Home'))
+        
     return render(request, 'Login.html')
+
+@login_required(login_url='Login')
+def Logout(request):
+    logout(request)
+    messages.success(request, '✅ Successfully Logged Out!')
+    return redirect(reverse('Login'))
+
+@login_required(login_url='Login')
+def Profile(request):
+    return render(request, 'Profile.html')

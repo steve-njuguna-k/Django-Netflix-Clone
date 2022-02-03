c4eaca958ba769ed9aa65651095a6916d349ed1e
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-03 19:55:27 +0300

    Added login page

--- Netflix/urls.py
+++ Netflix/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Home, name='Home'),
+    path('register', views.Register, name='Register'),
+    path('login', views.Login, name='Login'),
 ]
--- Netflix/views.py
+++ Netflix/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request, 'Index.html')
+
+def Register(request):
+    return render(request, 'Register.html')
+
+def Login(request):
+    return render(request, 'Login.html')

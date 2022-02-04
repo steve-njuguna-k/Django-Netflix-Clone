385c41c7b91bd9a1895526e7f8f05b4f74ef538d
Steve Njuguna <stevenjuguna93@gmail.com>
2022-02-04 15:32:01 +0300

    Added tv trailer to tv details page

--- Netflix/views.py
+++ Netflix/views.py
@@ -114,4 +114,12 @@ def TVDetails(request, tv_id):
     tv_details_results = tv_details_request.json()
     tv_details = tv_details_results
 
-    return render(request, 'TV Details.html', {'tv_details':tv_details, 'tv_id':tv_id, })
+    tv_video_request = requests.get("https://api.themoviedb.org/3/tv/" + str(tv_id) + "/videos?api_key=" + TMDB_API_KEY)
+    tv_video_results = tv_video_request.json()
+    tv_shows = tv_video_results['results']
+    newDict = dict()
+    for tv in tv_shows:
+        if tv['type'] == 'Trailer':
+            newDict['key'] = tv['key']
+
+    return render(request, 'TV Details.html', {'tv_details':tv_details, 'tv_id':tv_id, 'newDict':newDict})
